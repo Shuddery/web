328206fa23738bfc1426b171b2342fbf143e4b82
“vlad <vlad@gmail.com>
2020-12-20 14:55:34 +0300

    deleted laba 6

--- laba_6/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './main';
-import './style.css';
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
